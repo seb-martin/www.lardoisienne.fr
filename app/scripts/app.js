a47b5d64057482f2afe8978ce8f202b68a445d96
seb-martin <seb.martin.94@gmail.com>
2014-01-27 19:46:26 +0100

    Page de contact

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,23 +1,23 @@
 'use strict';
 
 angular.module('lardoisienneApp', [
-  'ngCookies',
-  'ngResource',
-  'ngSanitize',
-  'ngRoute'
-])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/contact', {
-        templateUrl: 'views/contact.html',
-            controller: 'MenuCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  })
+        'ngCookies',
+        'ngResource',
+        'ngSanitize',
+        'ngRoute'
+    ])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .when('/contact', {
+                templateUrl: 'views/contact.html',
+                controller: 'MenuCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    })
 ;
