286b532890612ec2a3b48211f5a08a9dd4845a7d
seb-martin <seb.martin.94@gmail.com>
2014-02-12 16:07:16 +0100

    Composant de texte éditable

--- app/scripts/components/components.js
+++ app/scripts/components/components.js
@@ -58,4 +58,75 @@ angular.module('lardoisienneApp')
             }
         };
     }])
+    .directive('ardEditableText', ['AuthService', function(authService) {
+        return {
+            restrict: 'EA',
+            templateUrl: 'views/components/editableText.html',
+            replace: true,
+            scope: {
+                text: '='
+            },
+            link: function (scope) {
+                // Event listeners
+                scope.$on('auth:loginSuccess', function (event, user) {
+                    scope.$apply(function () {
+                        scope.user = user;
+                    });
+                });
+                scope.$on('auth:logout', function () {
+                    scope.$apply(function () {
+                        scope.user = {};
+                    });
+                });
+
+                // Model
+                scope.user = authService.user;
+
+                scope.edition = false;
+
+
+                // Behaviours
+
+                var enterEdition = function () {
+                    scope.editedText = angular.copy(scope.text);
+                    scope.edition = true;
+                };
+
+                var validEdition = function () {
+                    scope.text = scope.editedText;
+                };
+
+                var exitEdition = function () {
+                    delete scope.editedText;
+                    scope.edition = false;
+                };
+
+                scope.enterEdition = function () {
+                    enterEdition();
+                };
+
+                scope.keypress = function (event) {
+                    console.log(event);
+                    if (event.keyCode === 13) { // Enter
+                        validEdition();
+                        exitEdition();
+                    } else if (event.keyCode === 27) { // Escape
+                        exitEdition();
+                    }
+                };
+
+
+                scope.ok = function () {
+                    validEdition();
+                    exitEdition();
+                };
+
+                scope.cancel = function () {
+                    exitEdition();
+                };
+
+
+            }
+        };
+    }])
 ;
--- app/scripts/controllers/contact.js
+++ app/scripts/controllers/contact.js
@@ -2,19 +2,40 @@
 'use strict';
 
 angular.module('lardoisienneApp')
-    .controller('ContactCtrl', ['$scope', '$firebase', /*'CloudData', */'SendMailService', function($scope, $firebase, /*CloudData, */SendMailService) {
+    .controller('ContactCtrl', ['$scope', '$firebase', 'AuthService', /*'CloudData', */'SendMailService', function($scope, $firebase, authService, /*CloudData, */SendMailService) {
 
         var ref = new Firebase('https://lardoisienne.firebaseio.com/contact');
-        $scope.contact = $firebase(ref);
+//        $scope.contact = $firebase(ref);
 
 //        $scope.contact = new CloudData('/contact');
 
-        var atelierTelRef = ref.child('atelier/coordonnees/tel');
-        var atelierTel = $firebase(atelierTelRef);
-        atelierTel.$bind($scope, 'tel');
+//        var atelierTelRef = ref.child('atelier/coordonnees/tel');
+//        var atelierTel = $firebase(atelierTelRef);
+//        atelierTel.$bind($scope, 'tel');
+        var cloudContact = $firebase(ref);
+        cloudContact.$bind($scope, 'contact');
 
+        $scope.user = authService.user;
 
+        $scope.$on('auth:loginSuccess', function (event, user) {
+            $scope.$apply(function () {
+                $scope.user = user;
+            });
+        });
+        $scope.$on('auth:logout', function () {
+            $scope.$apply(function () {
+                $scope.user = {};
+            });
+        });
 
+        $scope.editLigneAdresse = -1;
+        $scope.$watch('editLigneAdresse', function (newValue) {
+            console.log('editLigneAdresse', newValue);
+        });
+
+        $scope.editLigne = function(index) {
+            $scope.editLigneAdresse = index;
+        };
 
         $scope.data = {};
         $scope.reponse = undefined;
