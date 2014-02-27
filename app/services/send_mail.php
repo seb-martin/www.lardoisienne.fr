3a14275b27b0807c6a72562ccdafb07dbdbecbaa
seb-martin <seb.martin.94@gmail.com>
2014-02-28 00:52:55 +0100

    Correction du bug d'envoi de message

--- app/services/send_mail.php
+++ app/services/send_mail.php
@@ -11,13 +11,20 @@ if($stage == 'development'){
 
 
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    if (!isset($_POST['rs']) or empty($_POST['rs'])) {
+    // Validité de la raison sociale
+    $rs_valid = isset($_POST['rs']) and !empty($_POST['rs']);
+    // Validité de l'adresse mail de réponse
+    $replyTo_valid = isset($_POST['replyTo']) and !empty($_POST['replyTo']) and preg_match('#^[\w.-]+@[\w.-]+\.[a-zA-Z]{2,6}$#', $_POST['replyTo']);
+    // Validité du message
+    $message_valid = isset($_POST['message']) and !empty($_POST['message']);
+
+    if (!$rs_valid) {
         header("HTTP/1.0 400 Bad Request");
         echo "Pour que nous puissions vous identifier, merci de mentionner votre raison sociale.";
-    } elseif (!isset($_POST['replyTo']) or empty($_POST['replyTo']) or preg_match('#^[\w.-]+@[\w.-]+\.[a-zA-Z]{2,6}$#', $_POST['replyTo'])) {
+    } elseif (!$replyTo_valid) {
         header("HTTP/1.0 400 Bad Request");
         echo "Pour que nous puissions vous répondre, merci mentionner une adresse mail valide.";
-    } elseif (!isset($_POST['message']) or empty($_POST['message'])) {
+    } elseif (!$message_valid) {
         header("HTTP/1.0 400 Bad Request");
         echo "Pour que nous puissions vous informer, merci de préciser l'objet de votre message.";
     } else {
@@ -29,7 +36,8 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 
         // Le site est l'émetteur du mail
         $from = 'www@lardoisienne.fr';
-        // info est le destinataire du mail
+//         info est le destinataire du mail
+//        $to = 'seb.martin.94@gmail.com';
         $to = 'info@lardoisienne.fr';
 
         // Le sujet est construit avec la raison sociale et l'éventuel numéro de tel
