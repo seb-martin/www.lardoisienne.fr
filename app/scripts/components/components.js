0c34604396639e429fcb8535f96be7c1e2558ddf
seb-martin <seb.martin.94@gmail.com>
2014-05-27 22:35:07 +0200

    Mise en quarantaine de la directive ard-facebook due à l'issue #3

--- app/scripts/components/components.js
+++ app/scripts/components/components.js
@@ -278,10 +278,7 @@ angular.module('lardoisienneApp')
     .directive('ardFacebook', [function() {
         return {
             restrict: 'E',
-            template: '<a href="{{followurl}}" target="_blank">' +
-                '<img src="{{icon}}" alt="Facebook Icon"/>' +
-                '<span ng-transclude="true"></span>' +
-                '</a>',
+            template: '<a href="{{followurl}}" target="_blank"><img src="{{icon}}" alt="Facebook Icon"/><span ng-transclude="true"></span></a>',
             replace: true,
             transclude:true,
             scope: {
